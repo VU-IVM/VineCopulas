407d676a9337c5016562bb81543155793564666b
Claassen <j.n.claassen@vu.nl>
2024-03-04 09:01:13 +0100

    first version rtd

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ extensions = [
     "autoapi.extension",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    'sphinx.ext.mathjax',
+    'sphinx.ext.pngmath',
     "nbsphinx"   
 ]
 
@@ -80,4 +80,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
